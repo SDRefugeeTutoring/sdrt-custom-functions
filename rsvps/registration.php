d723e6b10ceb49499a852e6724caf1cd8f59255c
Matt Cromwell <reachme@mattcromwell.com>
2017-08-31 18:06:31 -0700

    Pull the email functions out for now, and add a nonce for added security purposes before updating post_meta

--- rsvps/registration.php
+++ rsvps/registration.php
@@ -1,33 +1,13 @@
 <?php
 add_action( 'setup_theme', function() {
-    $rsvpid = $_GET['rsvpid'];
-    $attended = $_GET['attended'];
 
-    $rsvpmeta = get_post_meta($rsvpid, '', true);
-    $volname = $rsvpmeta['volunteer_name'][0];
-    $volemail = $rsvpmeta['volunteer_email'][0];
-    //var_dump($rsvpmeta);
+	$nonce = ( isset( $_GET['_nonce']) ? $_GET['_nonce'] : '' );
+	$rsvpid = ( isset($_GET['rsvpid']) ? $_GET['rsvpid'] : '' );
+	$attended = ( isset($_GET['attended']) ? $_GET['attended'] : '' );
 
-    ob_start(); ?>
-    <h2>Hi <?php echo $volname; ?></h2>
-
-    <p>Thanks so much for attending tutoring yesterday.</p>
-
-    <?php
-    $message = ob_get_clean();
-
-    if ( current_user_can('update_plugins') ) {
-
-        if ( isset($rsvpid, $attended) ) {
-            update_post_meta( $rsvpid, 'attended', $attended );
-
-            $emailargs = array(
-                'toemail'       => $volemail,
-                'tosubject'     => 'New Email!!',
-                'emailcontent'  => $message
-            );
-
-            sdrt_trigger_email( $emailargs );
-        }
-    }
+	if ( current_user_can('update_plugins') ) {
+		if ( isset($rsvpid, $attended) && wp_verify_nonce( $nonce, 'sdrt_attendance_nonce' ) ) {
+			update_post_meta( $rsvpid, 'attended', $attended );
+		}
+	}
 });
