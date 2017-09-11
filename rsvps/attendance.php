88554bf280f3326dec56a996286e610641ab2639
mathetos <reachme@mattcromwell.com>
2017-09-10 22:08:07 -0700

    Add wp_email function to the attendance functionality.

--- rsvps/attendance.php
+++ rsvps/attendance.php
@@ -8,11 +8,20 @@ add_action( 'setup_theme', function() {
     $nonce = ( isset( $_GET['_nonce']) ? $_GET['_nonce'] : '' );
     $rsvpid = ( isset($_GET['rsvpid']) ? absint( $_GET['rsvpid'] ) : 0 );
     $attended = ( isset($_GET['attended']) ? $_GET['attended'] : '' );
+	$email = ( isset($_GET['email']) ? $_GET['email'] : '' );
+	$fname = ( isset($_GET['fname']) ? $_GET['fname'] : '' );
 
-    if ( 0 < $rsvpid && current_user_can('update_plugins') ) {
+    if ( current_user_can('update_plugins') ) {
         if ( isset($rsvpid, $attended) && wp_verify_nonce( $nonce, 'sdrt_attendance_nonce' ) ) {
             update_post_meta( $rsvpid, 'attended', $attended );
         }
     }
+
+    $event_title = get_the_title($rsvpid);
+
+	// The email we send the user to confirm they cleared and can now RSVP.
+	$headers[] = 'From: SD Refugee Tutoring <info@sdrefugeetutoring.com>';
+
+	wp_mail( $email, 'Thank you for attending ' . $event_title, sdrt_email_thankyou($fname, $event_title), $headers );
 
 });
