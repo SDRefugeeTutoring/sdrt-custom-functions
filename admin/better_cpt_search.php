1c543b066303b6f5f587e74324ebf902122cb3c3
Jason Adams <jason.adams@impress.org>
2021-11-02 21:56:07 -0700

    fix: searching rsvps works properly and limits to distinct RSVPs

--- admin/better_cpt_search.php
+++ admin/better_cpt_search.php
@@ -1,25 +1,33 @@
 <?php
 
-add_filter( 'posts_join', 'segnalazioni_search_join' );
-function segnalazioni_search_join ( $join ) {
-	global $pagenow, $wpdb;
+add_filter('posts_join', function($join, WP_Query $query) {
+    global $wpdb;
 
-	// I want the filter only when performing a search on edit page of Custom Post Type named "segnalazioni".
-	if ( is_admin() && 'edit.php' === $pagenow && 'rsvp' === $_GET['post_type'] && ! empty( $_GET['s'] ) ) {
-		$join .= 'LEFT JOIN ' . $wpdb->postmeta . ' ON ' . $wpdb->posts . '.ID = ' . $wpdb->postmeta . '.post_id ';
-	}
-	return $join;
-}
+    if ($query->is_admin && $query->is_search() && $query->query_vars['post_type'] === 'rsvp') {
+        $join .= 'LEFT JOIN ' . $wpdb->postmeta . ' ON ' . $wpdb->posts . '.ID = ' . $wpdb->postmeta . '.post_id ';
+    }
 
-add_filter( 'posts_where', 'segnalazioni_search_where' );
-function segnalazioni_search_where( $where ) {
-	global $pagenow, $wpdb;
+    return $join;
+}, 10, 2);
 
-	// I want the filter only when performing a search on edit page of Custom Post Type named "segnalazioni".
-	if ( is_admin() && 'edit.php' === $pagenow && 'rsvp' === $_GET['post_type'] && ! empty( $_GET['s'] ) ) {
-		$where = preg_replace(
-			"/\(\s*" . $wpdb->posts . ".post_title\s+LIKE\s*(\'[^\']+\')\s*\)/",
-			"(" . $wpdb->posts . ".post_title LIKE $1) OR (" . $wpdb->postmeta . ".meta_value LIKE $1)", $where );
-	}
-	return $where;
-}
+add_filter('posts_where', function($where, WP_Query $query) {
+    global $wpdb;
+
+    if ($query->is_admin && $query->is_search() && $query->query_vars['post_type'] === 'rsvp') {
+        $where = preg_replace(
+            "/\(\s*" . $wpdb->posts . ".post_title\s+LIKE\s*(\'[^\']+\')\s*\)/",
+            "(" . $wpdb->posts . ".post_title LIKE $1) OR (" . $wpdb->postmeta . ".meta_value LIKE $1)", $where);
+    }
+
+    return $where;
+}, 10, 2);
+
+add_filter('posts_groupby', function($groupBy, WP_Query $query) {
+    global $wpdb;
+
+    if ($query->is_admin && $query->is_search() && $query->query_vars['post_type'] === 'rsvp') {
+        return " $wpdb->posts.ID";
+    }
+
+    return $groupBy;
+}, 10, 2);
