58159d28d4750f23c2291f8bf88c72783650630e
Jason Adams <jason.adams@impress.org>
2022-02-12 21:26:54 -0800

    feature: add view function to load twig views

---
+++ src/helpers/views.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+use Twig\TwigFunction;
+
+function view(string $view, array $data = []): string
+{
+    static $twig = null;
+
+    if ( $twig === null ) {
+        $loader = new FilesystemLoader(SDRT_FUNCTIONS_DIR . '/src/views');
+
+        $twig = new Environment($loader, [
+            'cache' => wp_get_environment_type() === 'local' ? false : SDRT_FUNCTIONS_DIR . '/src/views/cache',
+        ]);
+
+        // WordPress functions
+        $twig->addFunction(new TwigFunction('isUserLoggedIn', 'is_user_logged_in'));
+        $twig->addFunction(new TwigFunction('siteUrl', 'site_url'));
+        $twig->addFunction(new TwigFunction('wpLoginUrl', 'wp_login_url'));
+        $twig->addFunction(new TwigFunction('getCurrentUserId', 'get_current_user_id'));
+        $twig->addFunction(new TwigFunction('getDeletePostLink', 'get_delete_post_link'));
+
+        $twig->addFunction(new TwigFunction('userCanRsvp', 'user_can_rsvp'));
+        $twig->addFunction(new TwigFunction('getUserRsvpForEvent', 'get_user_rsvp_for_event'));
+        $twig->addFunction(new TwigFunction('gravityForm', 'gravity_form'));
+    }
+
+    return $twig->load("$view.html.twig")->render($data);
+}
