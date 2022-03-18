035ae2ebc6565a92ce0b390cc5590ac5e69b8ac2
Jason Adams <jason.adams@impress.org>
2022-03-17 20:31:30 -0700

    feature: add even more logging capabilities including sending user information

--- src/Support/Log.php
+++ src/Support/Log.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace SDRT\CustomFunctions\Support;
 
+use Sentry\Severity;
 use Sentry\State\Scope;
 
 use function Sentry\captureMessage;
@@ -11,14 +12,48 @@ use function Sentry\withScope;
 
 class Log
 {
+    public static function fatal(string $message, array $context = []): void
+    {
+        self::sendToSentry($message, $context, Severity::fatal());
+    }
+
     public static function error(string $message, array $context = []): void
+    {
+        self::sendToSentry($message, $context, Severity::error());
+    }
+
+    public static function warning(string $message, array $context = []): void
+    {
+        self::sendToSentry($message, $context, Severity::warning());
+    }
+
+    public static function info(string $message, array $context = []): void
+    {
+        self::sendToSentry($message, $context, Severity::info());
+    }
+
+    public static function debug(string $message, array $context = []): void
+    {
+        self::sendToSentry($message, $context, Severity::debug());
+    }
+
+    private static function sendToSentry(string $message, array $context, Severity $severity): void
     {
         if ( ! self::sentryIsEnabled()) {
             return;
         }
 
-        withScope(function (Scope $scope) use ($message, $context): void {
+        withScope(function (Scope $scope) use ($message, $context, $severity): void {
             $scope->setContext('Context', $context);
+            $scope->setLevel($severity);
+
+            if (function_exists('is_user_logged_in') && is_user_logged_in() && ($user = wp_get_current_user())) {
+                $scope->setUser([
+                    'id' => $user->ID,
+                    'username' => $user->user_login,
+                    'email' => wp_get_current_user()->user_email,
+                ]);
+            }
 
             captureMessage("Log: $message");
         });
