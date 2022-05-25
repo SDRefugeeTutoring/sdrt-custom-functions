b8d72d8069963431d1e05b6d811ca9b2268e50d5
Jason Adams <jason.adams@impress.org>
2022-05-24 20:04:04 -0700

    refactor: rename Store to UserStore for clarity

--- src/VolunteerPortal/resources/js/store/index.tsx
+++ src/VolunteerPortal/resources/js/store/index.tsx
@@ -1,20 +1,20 @@
 import {createContext, ReactNode, useContext, useState} from 'react';
 import type User from '../types/User';
 
-interface Store {
+interface UserStore {
     user: User;
     setUser: (user: User | ((prevUser: User) => User)) => void;
 }
 
-const Store = createContext<Store>(null);
+const Store = createContext<UserStore>(null);
 
 export function UserContextProvider({children}: {children: ReactNode}) {
     const [user, setUser] = useState<User>(window.sdrtVolunteerPortal.user);
     return <Store.Provider value={{user, setUser}}>{children}</Store.Provider>;
 }
 
-export function useUserContext(): Store {
-    return useContext<Store>(Store);
+export function useUserContext(): UserStore {
+    return useContext<UserStore>(Store);
 }
 
 export default Store;
