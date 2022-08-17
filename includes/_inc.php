6e6cc394f04eb122daf1a98e2947e34dceded612
Jason Adams <jason.adams@impress.org>
2022-08-16 21:29:11 -0700

    refactor: load settings class via composer

--- includes/_inc.php
+++ includes/_inc.php
@@ -5,6 +5,3 @@ require_once( SDRT_FUNCTIONS_DIR . '/includes/sdrt_scripts.php');
 
 // Includes custom user meta functions
 require_once( SDRT_FUNCTIONS_DIR . '/includes/sdrt_user_meta.php');
-
-// Includes custom user meta functions
-require_once( SDRT_FUNCTIONS_DIR . '/includes/hd-wp-settings-api/class-hd-wp-settings-api.php');
